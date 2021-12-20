2c4c9a3f5b953034a20a1383b3e4c93381b1a2d0
Gustavo <gustavotremontsr@gmail.com>
2021-12-20 16:57:10 +0100

    pokeApp exercise 1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = (props) => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.getPokemon(e.target.pokemonName.value)
+    e.target.pokemonName.value = ''
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="pokemonName">Find a Pokemon</label>
+        <input type="text" name="pokemonName" id="pokemonName"/>
+
+        <input type="submit" value="Search" />
+      </form>
+    </section>
+  )
 };
 
 export default Form;
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-const PokemonList = () => {
-  return <div>PokemonList</div>;
+const PokemonList = ({pokemonData}) => {
+  return (
+    <section>
+      {
+        pokemonData.map(pokemon => 
+          <section key={uuidv4()} >
+              <p>{pokemon.name}</p>
+              <img src={pokemon.script} alt={pokemon.name} />
+          </section>
+        )
+      }
+    </section>
+  )
 };
 
 export default PokemonList;
